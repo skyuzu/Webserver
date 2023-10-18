366bc89b90aed308a33638d4c23b174138197cc5
Yiga <112139782+skyuzu@users.noreply.github.com>
2023-10-18 13:13:15 -0400

    commit

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 const WebSocket = require('ws');
+const http = require('http');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
-  // WebSocket connection established
-  ws.on('message', (message) => {
-    // Handle WebSocket messages from the client
-    console.log(`Received: ${message}`);
+const port = process.env.PORT || 3000;
 
-    // Broadcast the received data to all connected clients
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+app.use(express.static(path.join(__dirname, 'public')));
+
+wss.on('connection', (ws) => {
+  // Function to send random robot data to the client every 2000ms
+  function sendRandomRobotData() {
+    const robotData = generateRandomRobotData();
+    ws.send(JSON.stringify(robotData));
+  }
+
+  // Send initial data to the client when connected
+  sendRandomRobotData();
+
+  // Set up the interval to send updates
+  const updateInterval = setInterval(sendRandomRobotData, 2000);
+
+  // Handle client disconnect
+  ws.on('close', () => {
+    clearInterval(updateInterval);
   });
+});
+
+// Function to generate random robot data
+function generateRandomRobotData() {
+  const robotColors = ['red', 'blue', 'yellow', 'green'];
+  const randomRobotData = [];
+
+  for (let i = 0; i < 4; i++) {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    const direction = ['North', 'East', 'South', 'West'][Math.floor(Math.random() * 4)];
+    const color = robotColors[i];
+    randomRobotData.push({ x, y, direction, color });
+  }
+
+  return randomRobotData;
+}
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
